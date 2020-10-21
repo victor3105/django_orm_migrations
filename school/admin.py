b0c6e46344321701624f5d704480d5b42b664923
Victor Glebov <glebovv3105@gmail.com>
2020-10-21 14:34:43 -0600

    Changed to M2M relation. Edited the template

--- school/admin.py
+++ school/admin.py
@@ -3,11 +3,17 @@ from django.contrib import admin
 from .models import Student, Teacher
 
 
+class StudentTeacherInline(admin.TabularInline):
+    model = Student.teacher.through
+    extra = 0
+
+
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    pass
+    inlines = [StudentTeacherInline]
+    exclude = ['teacher']
 
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
-    pass
+    inlines = [StudentTeacherInline]
--- school/views.py
+++ school/views.py
@@ -11,5 +11,7 @@ def students_list(request):
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = 'group'
+    students = Student.objects.order_by(ordering).prefetch_related('teacher')
+    context['object_list'] = students
 
     return render(request, template, context)
